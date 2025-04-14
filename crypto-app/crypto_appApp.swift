e1b7b568682c85cdd3823d0f14c8a6332071ead1
Jothebug <hayenbg2603@gmail.com>
2025-04-14 21:25:10 +0700

    Initial Commit

---
+++ crypto-app/crypto_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_appApp.swift
+//  crypto-app
+//
+//  Created by HaYen on 14/4/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
