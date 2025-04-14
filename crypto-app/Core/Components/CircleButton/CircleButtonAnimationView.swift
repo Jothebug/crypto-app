48fa63f731ef3aa19319314a02bf5fb2ab5cad16
Jothebug <hayenbg2603@gmail.com>
2025-04-14 23:29:41 +0700

    implement header in HomeView

---
+++ crypto-app/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonAnimationView.swift
+//  crypto-app
+//
+//  Created by HaYen on 14/4/25.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView()
+}
---
+++ crypto-app/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  crypto-app
+//
+//  Created by HaYen on 14/4/25.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
